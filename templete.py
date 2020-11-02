24562a400ad80721534408a6237ea342d1a26ea3
Hsing-Ying Li <yy8secviielnce@gmail.com>
2020-11-02 15:42:25 +0800

    init

---
+++ templete.py
@@ -0,0 +1,34 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+class environment():
+    def __init__(self)
+
+
+
+class Ackley():
+    def __init__(self,args):
+
+        self.CROSS_RATE    = args.CROSS_RATE
+        self.MUTATE_RATE   = args.MUTATE_RATE
+        self.POP_SIZE      = args.POP_SIZE
+        self.N_GENERATIONS = args.N_GENERATIONS
+
+    def get_fitness(pred):
+        pass
+
+
+    def translateDNA(pop):
+        pass 
+
+
+    def select(pop, fitness):
+        pass 
+
+    def crossover(parent, pop):
+        
+        pass
+
+    def mutate(child):
+        pass
